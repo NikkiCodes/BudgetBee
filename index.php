de945a46b707cb082aad6147a584d9a723c660c0
me <me@toddlowmedia.com>
2022-12-04 22:08:23 -0500

    added bee image

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
     $companyName = "BudgetBee";
     $year = date("Y");
+?>
+    <div style='text-align:center;font-weight:bold;font-size:22px;'>
+        <?php echo $companyName; ?> - Coming Soon! 
+    </div>
+    <p style="text-align:center;">
+        <img src="bee.png" style="width:125px;">
+    </p>
 
-    echo "<div style='text-align:center;font-weight:bold;font-size:22px;'>{$companyName} - Coming Soon!</div>";
-
-    echo "<hr>";
-    ?>
     <p style="text-align:center;">
     Contact <?php echo $companyName; ?> with questions.
     <br>
