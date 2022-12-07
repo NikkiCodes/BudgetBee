27cdead7ef30e2ff87ae8158972017c378ea08de
me <me@toddlowmedia.com>
2022-12-06 20:41:07 -0500

    Added a paragraph

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
     <p style="text-align:center;">
         <img src="bee.png" style="width:125px;">
     </p>
+    <p style="text-align:left;"> <?php echo $companyName; ?> is a useful new program that will assist in simplifying your life.</p>
     <p style="text-align:center;">
         Contact <?php echo $companyName; ?> with questions.
-        <br>
+        <br> 
         copyright <?php echo $year; ?>
     </p>
