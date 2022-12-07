2e60ab3faa44ae7fd68740d60c05c5f45f4935b2
me <me@toddlowmedia.com>
2022-12-06 20:16:30 -0500

    Updated release date

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
     $companyName = "BudgetBee";
     $year = date("Y");
+    $releasedate = "25DEC2022";
 ?>
     <div style='text-align:center;font-weight:bold;font-size:22px;'>
-        <?php echo $companyName; ?> - Coming Soon! 
+        <?php echo $companyName . " - " . $releasedate; ?>  
     </div>
     <p style="text-align:center;">
         <img src="bee.png" style="width:125px;">
